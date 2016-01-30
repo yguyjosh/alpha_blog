4bee21bb87655f02d38ee8cabb14c2c2f1c78dc8
joshisen@me.com <isenhardt@yahoo.com>
2016-01-30 16:13:27 +0000

    Add edit update show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
+    
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save 
@@ -17,14 +26,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
  
- 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "The article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end  
+    
+  end
  
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
-    
-  
-
-  
 end
