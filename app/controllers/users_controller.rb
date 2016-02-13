29011466dce4e148a31dde1e59873b07c48d233f
joshisen@me.com <isenhardt@yahoo.com>
2016-02-13 20:11:20 +0000

    Add index to Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     
+    def index
+        @users = User.all    
+    end
+    
+    
     
     private
     def user_params
