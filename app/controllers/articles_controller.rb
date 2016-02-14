076248c942a48bd0d56d0b7fb3346c22fdfa1e44
joshisen@me.com <isenhardt@yahoo.com>
2016-02-14 04:24:05 +0000

    Added login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+
+  end
   def new
     @article = Article.new
   end
   
   def edit
     @article = Article.find(params[:id])
-  end
-  
-  def index
-    @articles = Article.all
-    
   end
   
   def create
